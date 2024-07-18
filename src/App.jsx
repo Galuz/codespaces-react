fd77f2c823f7d2b4786d6bb930ff5fcb4c5710f6
German Gonzalez <german.glz01@gmail.com>
2024-07-18 22:38:06 +0000

    Create components

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,13 @@
 import './App.css';
+import CardMovie from './components/CardMovie';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src="Octocat.png" className="App-logo" alt="logo" />
-        <p>
-          GitHub Codespaces <span className="heart">♥️</span> React
-        </p>
-        <p className="small">
-          Edit <code>src/App.jsx</code> and save to reload. testing changes
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </p>
-      </header>
+      <section>
+        <h1>holis</h1>
+        <CardMovie />
+      </section>
     </div>
   );
 }
---
+++ src/components/CardActor.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function CardActor() {
+  return (
+    <div>CardActor</div>
+  )
+}
