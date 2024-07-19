b2de5c42a00413f09e13ba4887333e7597bbec90
German Gonzalez <german.glz01@gmail.com>
2024-07-19 23:12:38 +0000

    Add images, add json and add base html structure for cardMovie component

--- src/components/CardMovie.jsx
+++ src/components/CardMovie.jsx
@@ -1,7 +1,11 @@
 import React from 'react'
+import belfastMovie from '../assets/belfast-movie.png';
 
 export default function CardMovie() {
   return (
-    <div>CardMovie</div>
+    <div className='card-movie'>
+      <img src={belfastMovie} alt="image movie" />
+      <span>Belfast</span>
+    </div>
   )
 }
