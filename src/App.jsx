dcb2775abca501f8896ea02bfe17c77a137283d0
German Gonzalez <german.glz01@gmail.com>
2024-07-24 03:59:40 +0000

    add images, routing and views

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,25 @@
+import React from 'react';
 import './App.css';
-import CardMovie from './components/CardMovie';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Navbar from './components/Navbar'; // Asegúrate de que la ruta es correcta
+import Home from './views/Home';
+import Ranking from './views/Ranking';
+import Rules from './views/Rules';
 
 function App() {
   return (
-    <div className="App">
-      <section>
-        <h1>holis</h1>
-        <CardMovie />
-      </section>
-    </div>
+    <Router>
+      <div className="App">
+        <Navbar />
+        <section>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/ranking" element={<Ranking />} />
+            <Route path="/rules" element={<Rules />} />
+          </Routes>
+        </section>
+      </div>
+    </Router>
   );
 }
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,31 @@
-import React from 'react'
+// Navbar.jsx
+import React from 'react';
+import { Link } from 'react-router-dom';
+import '../assets/styles/navbar.css'
+import oscarinLogo from '../assets/images/logos/Oscarin_Logo.png'
 
-export default function Navbar() {
+function Navbar() {
   return (
-    <div>Navbar</div>
-  )
+    <nav className="navbar">
+      <div className="navbar-logo">
+        <img src={oscarinLogo} alt="Oscar Icon" />
+      </div>
+      <ul className="navbar-menu">
+        <li><Link to="/">Mi Quiniela</Link></li>
+        <li><Link to="/ranking">Ranking</Link></li>
+        <li><Link to="/rules">Reglas</Link></li>
+      </ul>
+      <div className="navbar-actions">
+        <Link to="/compartir">
+          <img src="share_icon.png" alt="Compartir" />
+        </Link>
+        <Link to="/login">
+          <img src="twitter_icon.png" alt="Iniciar Sesión" />
+          Iniciar Sesión
+        </Link>
+      </div>
+    </nav>
+  );
 }
+
+export default Navbar;
