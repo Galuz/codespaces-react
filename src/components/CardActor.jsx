2ae53910868bef11553a2e4b4a58f46dea78d940
German Gonzalez <german.glz01@gmail.com>
2024-08-06 00:45:42 +0000

    change image path on json and add styles.

--- src/components/CardActor.jsx
+++ src/components/CardActor.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function CardActor({name, movie, image}) {
   return (
     <div className='card-actor'>
-      <img src="{image}" alt={`${name} image`}/>
+      <img src={image} alt={`${name} image`}/>
       <div>
         <p>{name}</p>
         <span>{movie}</span>
